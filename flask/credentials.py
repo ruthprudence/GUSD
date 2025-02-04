7a26906d9bc2f1d1b9e92909d0f6bc20e099d66f
Oracle Public Cloud User <opc@gusd.subnet01150017.vcn01150017.oraclevcn.com>
2025-02-04 06:30:57 +0000

    removed flask

--- flask/credentials.py
+++
@@ -1,3 +0,0 @@
-# This file should be ignored in your version control system
-
-DB_URI = 'mysql://gusd:gusdDATABASE!23d@hmysql.ruthprudence.com/gusd'
--- flask/gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-# gunicorn_config.py
-bind = "0.0.0.0:5000"
-workers = 3
-timeout = 120
-keepalive = 5
-worker_class = "sync"
-accesslog = "access.log"
-errorlog = "error.log"
